495df9db8cc807564c0031348d2f1d17e79aacdd
ValeriaVG <valeria.viana.gusmao@gmail.com>
2018-05-15 00:15:50 +0200

    Refactored constructor

--- index.js
+++ index.js
@@ -5,17 +5,13 @@ const Worker = require('./src/Worker')
 class PWM {
   constructor(payload = {}) {
     if (!payload.workers) payload.workers = 10
-
     const { path, workers, nextBatch, done, queue } = payload
-
     if (!path) {
       throw new Error('You must provide path to Worker')
     }
-
     if (!nextBatch && !queue) {
       throw new Error('You must provide or nextBatch function or queue array')
     }
-
     this.workers = []
     this.hasMore = true
     this.queue = new Queue()
@@ -23,26 +19,21 @@ class PWM {
     this.done = done
     this.isFilling = false
     if (!nextBatch) this.hasMore = false
-    if (queue && queue.length) {
-      this.queue.add(queue)
-    }
-    for (let i = 0; i < workers; i++) {
-      const worker = fork(path, [], { stdio: 'pipe' })
-      const workerLog = data => {
-        console.log(`worker #${i + 1}`, data.toString())
-      }
-      worker.stdout.on('data', workerLog)
-      worker.stderr.on('data', workerLog)
-      worker.on('message', message => {
-        this.next({ message, worker })
-      })
-
-      this.workers.push(worker)
-    }
-
+    if (queue && queue.length) this.queue.add(queue)
+    this.forkWorkers(path, workers)
     this.giveTasksToIdleWorkers()
   }
 
+  forkWorkers(path, n) {
+    for (let i = 0; i < n; i++) {
+      const worker = fork(path, [], { stdio: 'pipe' })
+      const workerLog = data => console.log(`worker #${i + 1}`, data.toString())
+      worker.stdout.on('data', workerLog)
+      worker.stderr.on('data', workerLog)
+      worker.on('message', message => this.next({ message, worker }))
+      this.workers.push(worker)
+    }
+  }
   async next({ message, worker }) {
     worker.busy = false
     if (this.done) this.done({ input: worker.input, output: message })
